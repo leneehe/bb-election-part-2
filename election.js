1249c03f628f5e796264fe76390532ffbe65d288
Lena <fatmeow@gmail.com>
2018-01-04 15:52:54 -0500

    Add vote to each candidate synchronously

---
+++ election.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var ulElection = document.querySelector('#election')
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(responseData){
+    responseData.candidates.forEach(function(candidate) {
+      var li = document.createElement('li')
+      li.innerHTML = '<b>' + candidate.name + ':</b> ' + candidate.votes + ' votes';
+      ulElection.append(li)
+
+      var form = document.createElement('form')
+      form.setAttribute('method', 'post')
+      form.setAttribute('action', 'https://bb-election-api.herokuapp.com/vote')
+      li.insertAdjacentElement('afterend', form);
+      var submitBtn = document.createElement('button')
+      var hiddenFld = document.createElement('input')
+      hiddenFld.setAttribute('hidden', 'true')
+      hiddenFld.setAttribute('name', 'name')
+      hiddenFld.setAttribute('value', candidate.name)
+      submitBtn.setAttribute('type', 'submit')
+      submitBtn.innerText = 'vote ' + candidate.name
+      form.insertAdjacentElement('beforeend', hiddenFld)
+      form.insertAdjacentElement('beforeend', submitBtn)
+    })
+  })
+/*
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/vote',
+    body: {'name': 'Gary'},
+    method: 'POST',
+    data: {},
+    dataType: 'json'
+  })
+*/
+
+});
